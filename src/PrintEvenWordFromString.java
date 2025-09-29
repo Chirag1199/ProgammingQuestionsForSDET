7caac53cfd1cc35e55c3d72a85ac0db70f716505
Chirag1199 <chirag.kataria.qa@gmail.com>
2025-09-29 20:25:54 +0530

    feat: Implement a logic for printing even words from the defined string

---
+++ src/PrintEvenWordFromString.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class PrintEvenWordFromString {
+    public static void main(String[] args) {
+
+
+        String str = "Sky is blue and vast";
+        String evenWords = "";
+
+        String[] data = str.split(" ");
+
+        System.out.println(Arrays.toString(data));
+
+        int dataLength = data.length;
+
+        for (int i = 0; i < dataLength; i++) {
+
+            String word = data[i];
+            if (word.length() % 2 == 0) {
+                evenWords += word + " ";
+
+            }
+
+        }
+
+        System.out.println(evenWords);
+
+    }
+}
