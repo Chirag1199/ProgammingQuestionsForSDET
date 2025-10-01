6445a4d9594a831be77371f478e908fa1942031b
Chirag1199 <chirag.kataria.qa@gmail.com>
2025-10-01 18:29:45 +0530

    feat(string): find most frequent character using HashMap

---
+++ src/MostFrequentElementInString.java
@@ -0,0 +1,51 @@
+import java.io.CharArrayReader;
+import java.util.*;
+
+public class MostFrequentElementInString {
+
+    static void repeatedCharInString(String str) {
+
+        String name = "Banana";
+        char[] c = name.toCharArray();
+        int n = c.length;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0;  i < n; i++) {
+
+            char ch = c[i];
+            if(!map.containsKey(ch)){
+                map.put(ch,1);
+            }
+            else{
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+
+        System.out.println(map);
+
+        // {a=3, B=1, n=2} --> Collections []
+
+        int maxFreq = 0; char repeatedChar = 0;
+
+        for( var v : map.entrySet())
+        {
+            if(v.getValue() > maxFreq){
+
+                maxFreq =  v.getValue();
+                repeatedChar = v.getKey();
+            }
+        }
+
+        System.out.println(repeatedChar);
+
+    }
+
+    public static void main(String[] args) {
+
+
+        repeatedCharInString("banana");
+
+    }
+
+}
