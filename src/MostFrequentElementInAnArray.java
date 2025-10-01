8fa94404e6f1472a90ccd58cca615ca0f053105b
Chirag1199 <chirag.kataria.qa@gmail.com>
2025-10-01 18:29:29 +0530

    feat(array): find most frequent element using HashMap

---
+++ src/MostFrequentElementInAnArray.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostFrequentElementInAnArray {
+
+    public static void main(String[] args) {
+
+        // Given an array, find the most frequent element in it. If there are
+        // multiple elements that appear a maximum number of times, print any one of them.
+        // n = 6 and arr[] = {1,3,2,1,4,1} => o/p = 1.
+
+        int[] arr = {1,3,2,1,4,1};
+        int n = arr.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            if (!map.containsKey(i)){
+                map.put(i,1);
+            }
+            else{
+                map.put(i, map.get(i) + 1);
+            }
+        }
+
+        System.out.println(map.entrySet());
+
+        int maxFreq = 0; int ansKey = 0;
+
+        for (var i : map.entrySet()) {
+            if (i.getValue() > maxFreq){
+                maxFreq = i.getValue();
+                ansKey = i.getKey();
+            }
+        }
+
+        System.out.println(ansKey);
+
+
+
+
+
+
+    }
+}
