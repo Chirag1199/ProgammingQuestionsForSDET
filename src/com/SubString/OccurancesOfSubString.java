9f1cc1f6555a2c53dd355cb3728e88f0980b7e21
Chirag1199 <chirag.kataria.qa@gmail.com>
2025-10-14 00:56:00 +0530

    feat(substring): generate all possible substrings and count specific substring occurrences

---
+++ src/com/SubString/OccurancesOfSubString.java
@@ -0,0 +1,49 @@
+package com.SubString;
+
+import java.util.ArrayList;
+
+public class OccurancesOfSubString {
+
+    public static void main(String[] args) {
+
+        // First Generate the substring.
+
+        String word = "abracadabra";
+        int n = word.length();
+
+        ArrayList<String> subStrings = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            String subStr = "";
+
+            for (int j = i; j < n; j++) {
+
+                subStr += word.charAt(j);
+                subStrings.add(subStr);
+
+            }
+
+        }
+        System.out.println(subStrings);
+        System.out.println(subStrings.size());
+
+        // traverse the array list and search for aa. if found, count the number of
+        // particular substring present in an arrayList.
+
+        int specificSubStringCount = 0;
+
+        for (int i = 0; i < subStrings.size(); i++){
+            String s = subStrings.get(i);
+
+            if (s.equals("abra")){
+                specificSubStringCount++;
+            }
+        }
+
+        System.out.println("The number of occurance of a specific substring is: "
+                + specificSubStringCount);
+
+
+    }
+}
